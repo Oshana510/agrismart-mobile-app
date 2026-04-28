d6beea9499b376b4dc36ebfc6a2cc5b444b8f426
IT24102901 <it24102901@my.sliit.lk>
2026-04-28 12:56:01 +0530

    Initialize models

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/mongoDB');
+const dns=require('dns');
 
 dotenv.config();
 
@@ -15,12 +16,28 @@ app.use(cors());
 app.use(express.json());
 
 // Import routes
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 
 // Use routes
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 
+// Basic route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'AgriSmart API is running!',
+    version: '1.0.0',
+    endpoints: {
+      auth: '/api/auth',
+      tasks: '/api/tasks',
+    }
+  });
+});
 
+dns.setServers(["0.0.0.0", "8.8.8.8"]);
 
 // Start server
 const PORT = process.env.PORT || 5000;
